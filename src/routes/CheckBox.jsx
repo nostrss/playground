dc7123fe6493d76afd8d07d3c8154b9a20891490
nostrss <jintagi@gmail.com>
2022-09-25 20:17:16 +0900

    install redux

--- src/routes/CheckBox.jsx
+++ src/routes/CheckBox.jsx
@@ -1,7 +1,26 @@
 export default function CheckBox() {
+  const onChangeCheck = (event) => {
+    alert(event.target.value);
+  };
+
   return (
     <div>
-      <h1>Welcome to CheckBox</h1>
+      <fieldset>
+        <legend>Welcome to CheckBox Playground</legend>
+        <div>
+          <input
+            type='checkbox'
+            name='first'
+            onChange={onChangeCheck}
+            value={'first'}
+          />
+          <label htmlFor='first'>first</label>
+        </div>
+        <div>
+          <input type='checkbox' name='second' />
+          <label htmlFor='second'>second</label>
+        </div>
+      </fieldset>
     </div>
   );
 }
