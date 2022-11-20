116a685989a376e70433e61ed4cd26bcd0d82a01
nostrss <jintagi@gmail.com>
2022-11-20 18:31:00 +0900

    back june

---
+++ algorithm/backjun/1008/1008.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+// let input = fs.readFileSync('1001.txt').toString().split(' ');
+let input = require('fs').readFileSync('1008.txt').toString().split('\n');
+
+console.log(Number(input[0]) / Number(input[1]));
