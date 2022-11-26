f120bcea46ce53cf04390e37d18bc4aa2cd0a5ad
nostrss <jintagi@gmail.com>
2022-11-26 16:19:43 +0900

    dark mode setting

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,11 +1,12 @@
+import GlobalStyle from 'GlobalStyle';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
