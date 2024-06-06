9ac5b3c0cfb72fb9b048b1c28b1c60e7ca0e5fd0
nostrss <jintagi@gmail.com>
2024-06-06 17:42:50 +0900

    add scss config

--- packages/vue-ui/vite.config.ts
+++ packages/vue-ui/vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/color.scss";`,
+      },
+    },
+  },
+
 })
