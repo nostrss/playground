fbc75fb420ed2bade2f75442e59d3991f0134d76
nostrss <jintagi@gmail.com>
2024-06-09 20:49:06 +0900

    fixed nuxt config

--- apps/nuxt3-playground/pages/modal/index.vue
+++ apps/nuxt3-playground/pages/modal/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import DialogModal from '@/components/modals/DialogModal.vue'
 const modalIsOpen = ref(false)
 const handleDialog = () => {
   modalIsOpen.value = !modalIsOpen.value
