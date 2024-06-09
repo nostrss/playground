32074fdd7d0ef4aef4c4453b5a4f804c1538cceb
nostrss <jintagi@gmail.com>
2024-06-09 19:46:44 +0900

    add dialog tag modal

---
+++ apps/nuxt3-playground/components/modals/DialogModal.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{
+  open: boolean
+}>()
+
+const modalRef = ref<HTMLDialogElement | null>(null)
+const handleDialog = () => {
+  modalRef.value?.open ? modalRef.value?.close() : modalRef.value?.showModal()
+}
+</script>
+
+<template>
+  <dialog ref="modalRef" :open="props.open">
+    <p>dialog contents</p>
+    <button @click="handleDialog">Close</button>
+  </dialog>
+</template>
+<style lang="scss" scoped></style>
---
+++ apps/nuxt3-playground/pages/modal/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const modalIsOpen = ref(false)
+const handleDialog = () => {
+  modalIsOpen.value = !modalIsOpen.value
+}
+</script>
+
+<template>
+  <div>
+    <button type="button" @click="handleDialog">Open Dialog</button>
+    <DialogModal :open="modalIsOpen"></DialogModal>
+  </div>
+</template>
+<style lang="scss" scoped></style>
