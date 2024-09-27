80c0674129c29343e54f960f91ef69bca85d8182
nostrss <jintagi@gmail.com>
2024-09-28 00:54:55 +0900

    feat: add page list

--- apps/nuxt3-playground/pages/index.vue
+++ apps/nuxt3-playground/pages/index.vue
@@ -7,6 +7,12 @@
     <li>
       <NuxtLink to="/render/noflex">render-no-flex</NuxtLink>
     </li>
+    <li>
+      <NuxtLink to="/speechapi/example1">speechapi page 1</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="/speechapi/example2">speechapi page 2</NuxtLink>
+    </li>
   </ul>
 </template>
 
