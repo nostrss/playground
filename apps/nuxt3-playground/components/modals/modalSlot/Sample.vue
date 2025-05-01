c5eca1aabef664dcd8d8f42418a7cc5fd951dc2d
nostrss <jintagi@gmail.com>
2025-05-02 01:36:48 +0900

    delete unused project

--- apps/nuxt3-playground/components/modals/modalSlot/Sample.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-  <p>dialog contents</p>
-  <button value="close">Close</button>
-  <button value="confirm">Confirm</button>
-</template>
-
-<style lang="scss" scoped></style>
